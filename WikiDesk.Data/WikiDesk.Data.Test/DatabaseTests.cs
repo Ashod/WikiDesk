e754a81a12c6138f92649ef8feebd7bc4e9e4fc9
Ashod Nakashian <ashodnakashian@yahoo.com>
2011-01-14 14:30:54 +0000

    * Added new Database unit-tests. * Data.Language is thoroughly covered by unit-tests.

---
+++ WikiDesk.Data/WikiDesk.Data.Test/DatabaseTests.cs
@@ -0,0 +1,54 @@
+﻿
+namespace WikiDesk.Data.Test
+{
+    using System.IO;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DatabaseTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            databaseFilename_ = Path.GetTempFileName();
+            database_ = new Database(databaseFilename_);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            database_.Dispose();
+
+            try
+            {
+                File.Delete(databaseFilename_);
+            }
+            catch
+            {
+            }
+        }
+
+        [Test]
+        public void SecondaryDatabaseInstance()
+        {
+            using (Database db = new Database(databaseFilename_))
+            {
+                Assert.NotNull(db);
+            }
+        }
+
+        protected Database Database
+        {
+            get { return database_; }
+        }
+
+        #region representation
+
+        private string databaseFilename_;
+
+        private Database database_;
+
+        #endregion // representation
+    }
+}
---
+++ WikiDesk.Data/WikiDesk.Data.Test/LanguageTableTests.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WikiDesk.Data.Test
+{
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LanguageTableTests : DatabaseTests
+    {
+        [Test]
+        public void Equality()
+        {
+            Language lang1 = new Language { Name = "english", Code = "en" };
+            Language lang2 = new Language { Name = "English", Code = "en" };
+            Assert.That(lang1.CompareTo(lang2) < 0);
+
+            lang2.Name = lang1.Name;
+            lang2.Code = "En";
+            Assert.That(lang1.CompareTo(lang2) < 0);
+
+            lang2.Name = lang1.Name;
+            lang2.Code = lang1.Code;
+            Assert.That(lang1.CompareTo(lang2) == 0);
+        }
+
+        [Test]
+        public void Insert()
+        {
+            Language langEn = new Language { Name = "english", Code = "en" };
+            Assert.AreEqual(1, Database.Insert(langEn));
+            Assert.AreEqual(langEn, Database.GetLanguageByName(langEn.Name));
+            Assert.AreEqual(langEn, Database.GetLanguageByCode(langEn.Code));
+
+            Language langDe = new Language { Name = "dutch", Code = "de" };
+            Assert.AreEqual(1, Database.Insert(langDe));
+            Assert.AreEqual(langDe, Database.GetLanguageByName(langDe.Name));
+            Assert.AreEqual(langDe, Database.GetLanguageByCode(langDe.Code));
+
+            IList<Language> languages = Database.GetLanguages();
+            Assert.NotNull(languages);
+            Assert.AreEqual(2, languages.Count);
+        }
+
+        [Test]
+        public void Update()
+        {
+            Language langEn = new Language { Name = "english", Code = "en" };
+            Assert.AreEqual(1, Database.Insert(langEn));
+
+            langEn.Name = "English";
+            langEn.Code = "En";
+            Assert.AreEqual(1, Database.Update(langEn));
+
+            Assert.AreEqual(langEn, Database.GetLanguageByName(langEn.Name));
+            Assert.AreEqual(langEn, Database.GetLanguageByCode(langEn.Code));
+
+            IList<Language> languages = Database.GetLanguages();
+            Assert.NotNull(languages);
+            Assert.AreEqual(1, languages.Count);
+        }
+
+        [Test]
+        public void UpdateInsert()
+        {
+            Language langEn = new Language { Name = "english", Code = "en" };
+            Assert.IsTrue(Database.UpdateInsert(langEn, null));
+            Assert.AreEqual(langEn, Database.GetLanguageByName(langEn.Name));
+            Assert.AreEqual(langEn, Database.GetLanguageByCode(langEn.Code));
+
+            Language langEng = new Language { Name = "English", Code = "En" };
+            Assert.IsFalse(Database.UpdateInsert(langEng, Database.GetLanguageByName(langEn.Name)));
+
+            Assert.AreEqual(langEng, Database.GetLanguageByName(langEng.Name));
+            Assert.AreEqual(langEng, Database.GetLanguageByCode(langEng.Code));
+
+            IList<Language> languages = Database.GetLanguages();
+            Assert.NotNull(languages);
+            Assert.AreEqual(1, languages.Count);
+        }
+
+        [Test]
+        public void Query()
+        {
+            Language langEn = new Language { Name = "english", Code = "en" };
+            Assert.AreEqual(1, Database.Insert(langEn));
+
+            Language langDe = new Language { Name = "dutch", Code = "de" };
+            Assert.AreEqual(1, Database.Insert(langDe));
+
+            Language langFr = new Language { Name = "french", Code = "fr" };
+            Assert.AreEqual(1, Database.Insert(langFr));
+
+            IList<Language> languages = Database.GetLanguages();
+            Assert.NotNull(languages);
+            Assert.AreEqual(3, languages.Count);
+
+            Assert.AreEqual(langEn, languages[0]);
+            Assert.AreEqual(langDe, languages[1]);
+            Assert.AreEqual(langFr, languages[2]);
+        }
+
+        [Test]
+        [ExpectedException(typeof(SQLite.SQLiteException))]
+        public void LanguageUniqueName()
+        {
+            Language langEn = new Language { Name = "english", Code = "en" };
+            Assert.AreEqual(1, Database.Insert(langEn));
+
+            Language langEng = new Language { Name = "English", Code = "en" };
+            Database.Insert(langEng);
+        }
+
+        [Test]
+        [ExpectedException(typeof(SQLite.SQLiteException))]
+        public void LanguageUniqueCode()
+        {
+            Language langEn = new Language { Name = "english", Code = "en" };
+            Assert.AreEqual(1, Database.Insert(langEn));
+
+            Language langEng = new Language { Name = "english", Code = "En" };
+            Database.Insert(langEng);
+        }
+    }
+}
