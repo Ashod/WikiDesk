b1a36f2e94bff899f99c588d51fec529208f74db
Ashod Nakashian <ashodnakashian@yahoo.com>
2011-04-08 18:05:53 +0000

    * Improved ExtractBlock. * New ExtractTag helpers. * New and updated unit-tests.

--- WikiDesk.Core/StringUtils.cs
+++ WikiDesk.Core/StringUtils.cs
@@ -1,6 +1,8 @@
 ﻿
 namespace WikiDesk.Core
 {
+    using System;
+    using System.Collections.Generic;
     using System.Text;
 
     public class StringUtils
@@ -46,6 +48,68 @@ namespace WikiDesk.Core
 
         /// <summary>
         /// Extracts the text between the start and end markers, if any.
+        /// Returns null if either marker were not found and offset is left as-is.
+        /// </summary>
+        /// <param name="text">The text to process.</param>
+        /// <param name="startMarker">The start marker.</param>
+        /// <param name="endMarker">The end marker.</param>
+        /// <param name="startOffset">
+        /// The offset to start searching from.
+        /// Contains the start index, on success, -1 on failure.
+        /// </param>
+        /// <param name="endOffset">Contains the end index, on success, -1 on failure.</param>
+        /// <returns>The text between the markers, if found, otherwise null.</returns>
+        public static string ExtractBlock(
+                                string text, 
+                                string startMarker,
+                                string endMarker,
+                                ref int startOffset,
+                                out int endOffset)
+        {
+            startOffset = text.IndexOf(startMarker, startOffset);
+            if (startOffset < 0)
+            {
+                startOffset = -1;
+                endOffset = -1;
+                return null;
+            }
+
+            int startIndex = startOffset + 1;
+            int endIndex = text.IndexOf(endMarker, startIndex);
+            if (endIndex < 0)
+            {
+                startOffset = -1;
+                endOffset = -1;
+                return null;
+            }
+
+            endOffset = endIndex + endMarker.Length - 1;
+            startIndex = startIndex + startMarker.Length - 1;
+            return text.Substring(startIndex, endIndex - startIndex);
+        }
+
+        /// <summary>
+        /// Extracts the text between the start and end markers, if any.
+        /// Returns null if either marker were not found and offset is left as-is.
+        /// </summary>
+        /// <param name="text">The text to process.</param>
+        /// <param name="startMarker">The start marker.</param>
+        /// <param name="endMarker">The end marker.</param>
+        /// <param name="offset">
+        /// The offset to start searching from. Contains the end index, of success.
+        /// Contains -1 on failure.
+        /// </param>
+        /// <returns>The text between the markers, if found, otherwise null.</returns>
+        public static string ExtractBlock(string text, string startMarker, string endMarker, ref int offset)
+        {
+            int endOffset;
+            string res = ExtractBlock(text, startMarker, endMarker, ref offset, out endOffset);
+            offset = Math.Max(offset, endOffset);
+            return res;
+        }
+
+        /// <summary>
+        /// Extracts the text between the start and end markers, if any.
         /// Returns null if either marker were not found.
         /// </summary>
         /// <param name="text">The text to process.</param>
@@ -54,20 +118,141 @@ namespace WikiDesk.Core
         /// <returns>The text between the markers, if found, otherwise null.</returns>
         public static string ExtractBlock(string text, string startMarker, string endMarker)
         {
-            int startIndex = text.IndexOf(startMarker);
-            if (startIndex < 0)
+            int offset = 0;
+            return ExtractBlock(text, startMarker, endMarker, ref offset);
+        }
+
+        /// <summary>
+        /// Given the contents of an opening tag (without brackets), it extracts the
+        /// name and attribs (if any) and states whether it's self-closing or not.
+        /// </summary>
+        /// <param name="text">The text to parse.</param>
+        /// <param name="attribs">The parsed attribs. Empty if none. Null if closing.</param>
+        /// <param name="closing">True if the tag is closing, otherwise False.</param>
+        /// <returns>The tag name.</returns>
+        public static string ExtractTagName(string text, out string attribs, out bool closing)
+        {
+            if (!string.IsNullOrEmpty(text))
             {
+                text = text.Trim();
+                closing = text.StartsWith("/");
+                if (closing)
+                {
+                    attribs = null;
+                    return text.TrimStart('/').Trim();
+                }
+
+                int index = text.IndexOf(' ');
+                if (index >= 0)
+                {
+                    attribs = text.Substring(index + 1);
+                    closing = attribs.EndsWith("/");
+                    if (closing)
+                    {
+                        attribs = attribs.TrimEnd('/');
+                    }
+
+                    return text.Substring(0, index);
+                }
+
+                attribs = string.Empty;
+                closing = text.EndsWith("/");
+                return closing ? text.TrimEnd('/') : text;
+            }
+
+            attribs = null;
+            closing = false;
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Extracts the contents of the first matching tag.
+        /// Returns null if self-closing, empty when there are no contents.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="offset"></param>
+        /// <param name="tag"></param>
+        /// <param name="attribs"></param>
+        /// <returns></returns>
+        public static string ExtractTag(string text, ref int offset, ref string tag, ref string attribs)
+        {
+            // Find the open tag.
+            int openIndex = offset;
+            string openTag = ExtractBlock(text, "<", ">", ref openIndex);
+            if (string.IsNullOrEmpty(openTag))
+            {
+                // No tags.
+                attribs = null;
                 return null;
             }
 
-            int endIndex = text.IndexOf(endMarker, startIndex + 1);
-            if (endIndex < 0)
+            bool closing;
+            tag = ExtractTagName(openTag, out attribs, out closing);
+            if (closing)
             {
+                offset = openIndex;
                 return null;
             }
 
-            startIndex += startMarker.Length;
-            return text.Substring(startIndex, endIndex - startIndex);
+            // Search for the close tag.
+            int closeIndex = openIndex + openTag.Length;
+
+            while (closeIndex >= 0 && closeIndex < text.Length)
+            {
+                int endIndex;
+                string closeTag = ExtractBlock(text, "<", ">", ref closeIndex, out endIndex);
+                if (closeIndex < 0)
+                {
+                    // No tags, process all of it.
+                    attribs = null;
+                    return null;
+                }
+
+                string dummy;
+                string closingTagName = ExtractTagName(closeTag, out dummy, out closing);
+                if (closing && string.Compare(tag, closingTagName, true) == 0)
+                {
+                    // Found.
+                    offset = endIndex;
+                    return text.Substring(openIndex + 1, closeIndex - openIndex - 1);
+                }
+            }
+
+            offset = -1;
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Extracts the contents of the
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="offset"></param>
+        /// <param name="attribs"></param>
+        /// <returns></returns>
+        public static string ExtractTagContents(string text, ref int offset, out List<KeyValuePair<string, string>> attribs)
+        {
+            // Find the open tag.
+            int openIndex = offset;
+            string openTag = ExtractBlock(text, "<", ">", ref openIndex);
+            if (openIndex == 0)
+            {
+                // No tags.
+                attribs = null;
+                return null;
+            }
+
+            // Search for the close tag.
+            int closeIndex = 0;
+            string closeTag = ExtractBlock(text, "<", ">", ref closeIndex);
+            if (closeIndex == 0)
+            {
+                // No tags, process all of it.
+                attribs = null;
+                return null;
+            }
+
+            attribs = null;
+            return string.Empty;
         }
     }
 }
--- WikiDesk.Core/WikiDesk.Core.Test/StringUtilsTest.cs
+++ WikiDesk.Core/WikiDesk.Core.Test/StringUtilsTest.cs
@@ -5,6 +5,8 @@
     [TestFixture]
     public class StringUtilsTest
     {
+        #region RemoveBlocks
+
         [Test]
         public void RemoveComents()
         {
@@ -36,5 +38,170 @@
             const string EXP = "blik more text bzz";
             Assert.AreEqual(EXP, StringUtils.RemoveBlocks(RAW, "<!--", "-->"));
         }
+
+        #endregion // RemoveBlocks
+
+        #region ExtractBlock
+
+        [Test]
+        public void ExtractBlockSimple()
+        {
+            const string RAW = "blik <!--comment--> more text <!----> bzz";
+            const string EXP = "comment";
+            Assert.AreEqual(EXP, StringUtils.ExtractBlock(RAW, "<!--", "-->"));
+        }
+
+        [Test]
+        public void ExtractBlockOffset()
+        {
+            const string RAW = "blik <!--comment--> more text <!----> bzz";
+
+            int offset = 20;
+            Assert.AreEqual(string.Empty, StringUtils.ExtractBlock(RAW, "<!--", "-->", ref offset));
+            Assert.AreEqual(36, offset);
+        }
+
+        [Test]
+        public void ExtractBlockStartEnd()
+        {
+            const string RAW = "blik <!--comment--> more text <!----> bzz";
+
+            int startOffset = 20;
+            int endOffset;
+            Assert.AreEqual(string.Empty, StringUtils.ExtractBlock(RAW, "<!--", "-->", ref startOffset, out endOffset));
+            Assert.AreEqual(30, startOffset);
+            Assert.AreEqual(36, endOffset);
+        }
+
+        #endregion // ExtractBlock
+
+        #region ExtractTagName
+
+        [Test]
+        public void ExtractTagNameB()
+        {
+            const string RAW = "b";
+            const string EXP = "b";
+            bool closing;
+            string attribs;
+            Assert.AreEqual(EXP, StringUtils.ExtractTagName(RAW, out attribs, out closing));
+            Assert.False(closing);
+            Assert.IsEmpty(attribs);
+        }
+
+        [Test]
+        public void ExtractTagNameBr()
+        {
+            const string RAW = "br/";
+            const string EXP = "br";
+            bool closing;
+            string attribs;
+            Assert.AreEqual(EXP, StringUtils.ExtractTagName(RAW, out attribs, out closing));
+            Assert.True(closing);
+            Assert.IsEmpty(attribs);
+        }
+
+        [Test]
+        public void ExtractTagNameBrSpaced()
+        {
+            const string RAW = "br /";
+            const string EXP = "br";
+            bool closing;
+            string attribs;
+            Assert.AreEqual(EXP, StringUtils.ExtractTagName(RAW, out attribs, out closing));
+            Assert.True(closing);
+            Assert.IsEmpty(attribs);
+        }
+
+        [Test]
+        public void ExtractTagNameAttribs()
+        {
+            const string RAW = "tag name=\"kikos\" ";
+            const string EXP = "tag";
+            bool closing;
+            string attribs;
+            Assert.AreEqual(EXP, StringUtils.ExtractTagName(RAW, out attribs, out closing));
+            Assert.False(closing);
+            Assert.AreEqual("name=\"kikos\"", attribs);
+        }
+
+        [Test]
+        public void ExtractTagNameEmpty()
+        {
+            bool closing;
+            string attribs;
+            Assert.AreEqual(string.Empty, StringUtils.ExtractTagName(string.Empty, out attribs, out closing));
+            Assert.False(closing);
+            Assert.IsNull(attribs);
+        }
+
+        [Test]
+        public void ExtractTagNameNull()
+        {
+            bool closing;
+            string attribs;
+            Assert.AreEqual(string.Empty, StringUtils.ExtractTagName(null, out attribs, out closing));
+            Assert.False(closing);
+            Assert.IsNull(attribs);
+        }
+
+        [Test]
+        public void ExtractTagNameClose()
+        {
+            const string RAW = "/b";
+            const string EXP = "b";
+            bool closing;
+            string attribs;
+            Assert.AreEqual(EXP, StringUtils.ExtractTagName(RAW, out attribs, out closing));
+            Assert.True(closing);
+            Assert.IsNull(attribs);
+        }
+
+        #endregion // ExtractTagName
+
+        #region ExtractTag
+
+        [Test]
+        public void ExtractTagSelfClosing()
+        {
+            const string RAW = "blik more text <br />";
+            int offset = 0;
+            string tag = null;
+            string attribs = null;
+            Assert.AreEqual(null, StringUtils.ExtractTag(RAW, ref offset, ref tag, ref attribs));
+
+            Assert.AreEqual("br", tag);
+            Assert.IsEmpty(attribs);
+            Assert.AreEqual(RAW.Length - 1, offset);
+        }
+
+        [Test]
+        public void ExtractTagB()
+        {
+            const string RAW = "blik more text <b>bzz</b>";
+            const string EXP = "bzz";
+            int offset = 0;
+            string tag = null;
+            string attribs = null;
+            Assert.AreEqual(EXP, StringUtils.ExtractTag(RAW, ref offset, ref tag, ref attribs));
+
+            Assert.AreEqual("b", tag);
+            Assert.IsEmpty(attribs);
+            Assert.AreEqual(RAW.Length - 1, offset);
+        }
+
+        [Test]
+        [Ignore]
+        public void ExtractTag()
+        {
+            const string RAW = "blik more text <b>bzz</b>";
+            const string EXP = "bzz";
+            int offset = 0;
+            string tag = null;
+            string attribs = null;
+            Assert.AreEqual(EXP, StringUtils.ExtractTag(RAW, ref offset, ref tag, ref attribs));
+        }
+
+        #endregion // ExtractTag
     }
 }
