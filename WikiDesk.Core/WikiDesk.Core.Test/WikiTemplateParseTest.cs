3c543e004fbef9fa476f3e7b96050317dfd19640
Ashod Nakashian <ashodnakashian@yahoo.com>
2011-06-24 20:52:24 +0000

    Added copyright and license information.

--- WikiDesk.Core/WikiDesk.Core.Test/WikiTemplateParseTest.cs
+++ WikiDesk.Core/WikiDesk.Core.Test/WikiTemplateParseTest.cs
@@ -1,4 +1,40 @@
-﻿namespace WikiDesk.Core.Test
+﻿// -----------------------------------------------------------------------------------------
+// <copyright file="WikiTemplateParseTest.cs" company="ashodnakashian.com">
+//
+// This file is part of WikiDesk.
+// Copyright (C) 2010, 2011 Ashod Nakashian
+// https://github.com/Ashod/WikiDesk
+//
+//  WikiDesk is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  WikiDesk is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with WikiDesk. If not, see http://www.gnu.org/licenses/.
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary>
+//   Defines the WikiTemplateParseTest type.
+// </summary>
+// -----------------------------------------------------------------------------------------
+
+namespace WikiDesk.Core.Test
 {
     using System.Collections.Generic;
     using System.IO;
--- WikiDesk.Core/WikiSite.cs
+++ WikiDesk.Core/WikiSite.cs
@@ -1,4 +1,44 @@
-﻿
+﻿// -----------------------------------------------------------------------------------------
+// <copyright file="WikiSite.cs" company="ashodnakashian.com">
+//
+// This file is part of WikiDesk.
+// Copyright (C) 2010, 2011 Ashod Nakashian
+// https://github.com/Ashod/WikiDesk
+//
+//  WikiDesk is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  WikiDesk is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with WikiDesk. If not, see http://www.gnu.org/licenses/.
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary>
+//   Represents a Wiki Site with all its info and attributes.
+//   A Wiki Site is a unique language.domain pair, such as en.wikipedia,
+//   hy.wiktionary, simple.wikibooks, fr.wikiquote or sr.wikinews.
+//   Since each on-line site has its own settings, templates and magic words,
+//   here is where we define those things and bring them together.
+//   WikiSite emulates on-line wiki sites.
+// </summary>
+// -----------------------------------------------------------------------------------------
+
 namespace WikiDesk.Core
 {
     using System;
