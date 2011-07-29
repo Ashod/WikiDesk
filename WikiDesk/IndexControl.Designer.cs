54aa3f6f47713d4900c2719183211bb16c8b116e
Ashod Nakashian <ashodnakashian@yahoo.com>
2011-07-29 23:42:09 +0500

    Index and Search controls get icons.

--- WikiDesk/IndexControl.Designer.cs
+++ WikiDesk/IndexControl.Designer.cs
@@ -29,6 +29,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IndexControl));
             this.cboDomains_ = new System.Windows.Forms.ComboBox();
             this.txtTitle_ = new System.Windows.Forms.TextBox();
             this.lstTitles_ = new System.Windows.Forms.ListView();
@@ -42,10 +43,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cboDomains_.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboDomains_.FormattingEnabled = true;
-            this.cboDomains_.Location = new System.Drawing.Point(11, 11);
+            this.cboDomains_.Location = new System.Drawing.Point(6, 6);
             this.cboDomains_.MaxDropDownItems = 10;
             this.cboDomains_.Name = "cboDomains_";
-            this.cboDomains_.Size = new System.Drawing.Size(308, 21);
+            this.cboDomains_.Size = new System.Drawing.Size(318, 21);
             this.cboDomains_.TabIndex = 1;
             this.cboDomains_.SelectedIndexChanged += new System.EventHandler(this.cboDomains__SelectedIndexChanged);
             //
@@ -53,9 +54,9 @@
             //
             this.txtTitle_.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtTitle_.Location = new System.Drawing.Point(11, 65);
+            this.txtTitle_.Location = new System.Drawing.Point(6, 60);
             this.txtTitle_.Name = "txtTitle_";
-            this.txtTitle_.Size = new System.Drawing.Size(308, 20);
+            this.txtTitle_.Size = new System.Drawing.Size(318, 20);
             this.txtTitle_.TabIndex = 2;
             this.txtTitle_.TextChanged += new System.EventHandler(this.txtTitle__TextChanged);
             this.txtTitle_.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtTitle__KeyDown);
@@ -70,11 +71,11 @@
             this.lstTitles_.FullRowSelect = true;
             this.lstTitles_.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
             this.lstTitles_.HideSelection = false;
-            this.lstTitles_.Location = new System.Drawing.Point(11, 91);
+            this.lstTitles_.Location = new System.Drawing.Point(6, 86);
             this.lstTitles_.MultiSelect = false;
             this.lstTitles_.Name = "lstTitles_";
             this.lstTitles_.ShowItemToolTips = true;
-            this.lstTitles_.Size = new System.Drawing.Size(308, 296);
+            this.lstTitles_.Size = new System.Drawing.Size(318, 306);
             this.lstTitles_.TabIndex = 3;
             this.lstTitles_.UseCompatibleStateImageBehavior = false;
             this.lstTitles_.View = System.Windows.Forms.View.Details;
@@ -95,9 +96,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cboLanguages_.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboLanguages_.FormattingEnabled = true;
-            this.cboLanguages_.Location = new System.Drawing.Point(11, 38);
+            this.cboLanguages_.Location = new System.Drawing.Point(6, 33);
             this.cboLanguages_.Name = "cboLanguages_";
-            this.cboLanguages_.Size = new System.Drawing.Size(308, 21);
+            this.cboLanguages_.Size = new System.Drawing.Size(318, 21);
             this.cboLanguages_.TabIndex = 4;
             this.cboLanguages_.SelectedIndexChanged += new System.EventHandler(this.cboLanguages__SelectedIndexChanged);
             //
@@ -116,8 +117,8 @@
             | WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom)));
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HideOnClose = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "IndexControl";
-            this.Padding = new System.Windows.Forms.Padding(5);
             this.Text = "Index";
             this.ResumeLayout(false);
             this.PerformLayout();
