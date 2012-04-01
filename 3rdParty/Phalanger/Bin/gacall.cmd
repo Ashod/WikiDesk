2d0a4e9b918b39d432718f811d3669385e577b12
Ashod Nakashian <ashodnakashian@yahoo.com>
2012-04-01 12:55:14 +0500

    Phalanger 3.0 2012-03-24 r3.

---
+++ 3rdParty/Phalanger/Bin/gacall.cmd
@@ -0,0 +1,9 @@
+PATH = %PATH%;"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools"
+
+gacutil -f -i ShmChannel.dll
+gacutil -f -i PhpNetCore.dll
+gacutil -f -i PhpNetClassLibrary.dll
+gacutil -f -i PhpNetMsSql.dll
+gacutil -f -i PhpNetXmlDom.dll
+gacutil -f -i php4ts.dll
+gacutil -f -i php5ts.dll
