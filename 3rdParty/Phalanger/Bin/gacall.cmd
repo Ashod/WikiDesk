310cecc3a11ea253b0f986ae2f5770921b2ff79e
Ashod Nakashian <ashodnakashian@yahoo.com>
2012-09-03 16:14:46 -0700

    Added own build of Phalanger based on Phalanger r98336 (after release 3.0 2012-06-13).
    Fixes:
    Support for GMT time zone added.
    DateTime parsing is now implemented.

--- 3rdParty/Phalanger/Bin/gacall.cmd
+++ 3rdParty/Phalanger/Bin/gacall.cmd
@@ -1,8 +1,5 @@
-PATH = %PATH%;"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools"
+PATH = "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\";%PATH%
 
-gacutil -f -i PhpNetCore.dll
-gacutil -f -i PhpNetClassLibrary.dll
-gacutil -f -i PhpNetMsSql.dll
-gacutil -f -i PhpNetXmlDom.dll
-gacutil -f -i php4ts.dll
-gacutil -f -i php5ts.dll
+
+
+for %%i in (*.dll) do gacutil -f -i %%i
